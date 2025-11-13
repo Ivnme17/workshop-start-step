d48003b443741ab57d6c41b71e8a7c1fdd469618
Ivnm17 <Ivnm17@gmail.com>
2025-11-13 11:58:51 +0100

    feat: add email subscription functionality with Resend integration

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,11 @@
+import { Resend } from 'resend';
+import { RESEND_API_KEY, FROM_EMAIL, TO_EMAIL } from 'astro:env/server'
 import { defineAction } from "astro:actions";
 import { addLike, getLikes } from "./repository";
 import type { LikesResponse } from "./model";
+import { z } from 'astro:schema';
 
+const resend = new Resend(RESEND_API_KEY);
 
 export const server = {
     addLike: defineAction<LikesResponse>({
@@ -12,6 +16,27 @@ export const server = {
     getLikes: defineAction<LikesResponse>({
         async handler(slug){
             return { likes: await getLikes(slug) };
+        }
+    }),
+    sendSubcription: defineAction({
+        accept: 'form',
+        input: z.object({
+            email: z.string().email('Invdalid email'),
+        }),
+        handler: async input => {
+            try{
+                const { email} = input;
+                await resend.emails.send({
+                    from: FROM_EMAIL,
+                    to: TO_EMAIL,
+                    subject: "Hello World",
+                    html: `<p>Congrats on sending your <strong>${email}</strong></p>`
+                });
+
+                return {success: true, message: 'E-mail sent successfully :D' };
+            }catch(error){
+                return {success: false, message: 'There was an error sending the e-mail :C'};
+            }
         }
     }),
 };

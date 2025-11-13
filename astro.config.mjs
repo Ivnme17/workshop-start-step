2d5234142d2d0f5b092d9f9da2157d8be295bbf2
Ivnm17 <Ivnm17@gmail.com>
2025-11-13 11:29:53 +0100

    feat: implement like button functionality with server actions
    - Added @astrojs/node dependency for server actions.
    - Updated LikeButton component to accept a slug prop and fetch initial like count from the server.
    - Created server actions for adding and retrieving likes in index.ts.
    - Defined LikesResponse type for consistent response structure.
    - Implemented in-memory like storage in repository.ts to manage likes per post.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations:[react()],
   vite: {
     plugins: /**@type { any }*/ ([tailwindcss()]),
   },
+  adapter: node({
+    mode:'standalone'
+  }),
   env: {
     schema: {
       CONTENT_ISLAND_SECRET_TOKEN: envField.string({
---
+++ src/actions/index.ts
@@ -0,0 +1,17 @@
+import { defineAction } from "astro:actions";
+import { addLike, getLikes } from "./repository";
+import type { LikesResponse } from "./model";
+
+
+export const server = {
+    addLike: defineAction<LikesResponse>({
+        async handler(slug){
+            return { likes: await addLike(slug) };
+        },
+        }),
+    getLikes: defineAction<LikesResponse>({
+        async handler(slug){
+            return { likes: await getLikes(slug) };
+        }
+    }),
+};
